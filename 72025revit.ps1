5a10c35f27509865eca5b4a777fe731d052652d5
rkohalmy <165615874+rkohalmy@users.noreply.github.com>
2025-07-07 10:50:15 -0700

    Update 72025revit.ps1
    HKLM:

--- 72025revit.ps1
+++ 72025revit.ps1
@@ -6,7 +6,7 @@ Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanWorkstatio
 
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanWorkstation\Parameters" -Name "RequireSecuritySignature" -Value 1 
 
-Set-ItemProperty -Path "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" -Name "RestrictAnonymous" -Value 1
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa" -Name "RestrictAnonymous" -Value 1
 
 $latestVCURL = "https://aka.ms/vs/17/release/vc_redist.x64.exe"
 $installerPath = "$env:TEMP\vc_redist.x64.exe"
