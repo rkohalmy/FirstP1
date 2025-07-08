824df51e519bdf46512cc438e9b3629a2e302fac
rkohalmy <165615874+rkohalmy@users.noreply.github.com>
2025-07-08 09:24:31 -0700

    Update 72025revit.ps1
    3D and comments.

--- 72025revit.ps1
+++ 72025revit.ps1
@@ -1,3 +1,4 @@
+#SMB and Null Session correction
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" -Name "EnableSecuritySignature" -Value 1
 
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" -Name "RequireSecuritySignature" -Value 1 
@@ -8,6 +9,7 @@ Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanWorkstatio
 
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa" -Name "RestrictAnonymous" -Value 1
 
+#Install 14.44 CPP Redis
 $latestVCURL = "https://aka.ms/vs/17/release/vc_redist.x64.exe"
 $installerPath = "$env:TEMP\vc_redist.x64.exe"
 
@@ -15,5 +17,8 @@ Invoke-WebRequest -Uri $latestVCURL -OutFile $installerPath
 
 Start-Process -FilePath $installerPath -ArgumentList "/install", "/quiet", "/norestart" -Wait
 
+# Birthday attack
+Disable-TlsCipherSuite -Name 'TLS_RSA_WITH_3DES_EDE_CBC_SHA'
 
+# 3D Viewer remover
 Get-appxprovisionedpackage –online | where-object {$_.packagename –like “*Microsoft.Microsoft3DViewer*”} | Remove-AppxProvisionedPackage -online
